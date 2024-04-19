be9ce053a7a0aeb6c2caf9fc6a38468753812e45
Sndubey <103138666+Sndubey@users.noreply.github.com>
2024-04-19 14:41:51 +0530

    Home Page

---
+++ Home.aspx.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace F_F
+{
+    public partial class H1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            Response.Redirect("Order/D_order_pg.aspx");
+        }
+
+        protected void Button2_Click(object sender, EventArgs e)
+        {
+            Response.Redirect("Order/Petrol_Pg.aspx");
+        }
+
+        protected void Button3_Click(object sender, EventArgs e)
+        {
+            Response.Redirect("Order/Order_pg.aspx");
+        }
+    }
+}
