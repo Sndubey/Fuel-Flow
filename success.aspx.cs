a2e5287d11abb5147ec490b81aa519b8bf1f178f
Sndubey <103138666+Sndubey@users.noreply.github.com>
2024-04-19 14:50:13 +0530

    This Success Page
    This page apper affter successful payment is done.

---
+++ success.aspx.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Configuration; // Add this for ConfigurationManager
+using System.Data.SqlClient; // Add this for SqlConnection and other SQL-related classes
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace F_F
+{
+    public partial class success : System.Web.UI.Page
+    {
+        string connectionString = ConfigurationManager.ConnectionStrings["YourConnectionStringName"].ConnectionString;
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            string username = Session["username"].ToString();
+            string orderId = Request.QueryString["orderId"];
+            string paymentId = Request.QueryString["paymentId"];
+
+            // Establish connection to the SQL Server database
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                // Define the SQL query with parameters to avoid SQL injection
+                string query = "INSERT INTO Payment (Username, OrderId, PaymentId) VALUES (@Username, @OrderId, @PaymentId)";
+
+                // Create and open the SqlCommand object
+                using (SqlCommand command = new SqlCommand(query, connection))
+                {
+                    // Add parameters to the SqlCommand object
+                    command.Parameters.AddWithValue("@Username", username);
+                    command.Parameters.AddWithValue("@OrderId", orderId);
+                    command.Parameters.AddWithValue("@PaymentId", paymentId);
+
+                    // Open the database connection
+                    connection.Open();
+
+                    // Execute the SQL query
+                    command.ExecuteNonQuery();
+                }
+            }
+
+            // Display the orderId and paymentId
+            Label1.Text= username;
+            Label2.Text = orderId;
+            Label3.Text = paymentId;
+        }
+
+        
+    }
+}
