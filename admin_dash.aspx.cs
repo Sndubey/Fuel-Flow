2ca237ac5941db6fd4f867b3e5a79f15bc5b0037
Sndubey <103138666+Sndubey@users.noreply.github.com>
2024-04-19 14:33:07 +0530

    Admin dashboard page

---
+++ admin_dash.aspx.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace F_F
+{
+    public partial class admin_dash : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (Session["username"] != null)
+            {
+                string username = Session["username"].ToString();
+            }
+            else
+            {
+                Response.Write("Enter correcct login credentials");
+            }
+        }
+    }
+}
